ff963b2297cfbbf548ce4c5b3944c472cfaad842
gaha20 <128089822+gaha20@users.noreply.github.com>
2023-07-04 18:22:34 +0000

    task and counter creation

--- src/js/component/input.jsx
+++ src/js/component/input.jsx
@@ -3,12 +3,13 @@ import React, {useState} from "react";
 const Input = () => {
     const [data, setData] = useState("")
     const [toDo, setToDo] = useState([])
+    const toDoCounter = toDo.length
     return (
-        <div className="container justify-content-center align-items-center vh-100">
-            <h1 className="text-light">toDo</h1>
-            <label htmlFor="toDo" className="text-light"> toDo:</label>
+        <div className="container justify-content-center align-items-center vh-100 vw-50">
+            <h1 className="text-light confortaa">2Do´s</h1>
+            <label htmlFor="toDo" className="text-light confortaa p-1"> task: </label>
             <input
-            className="bg-secondary text-light"
+            className="bg-secondary text-light confortaa"
             type="text"
             id="toDo"
             value={data}
@@ -26,13 +27,13 @@ const Input = () => {
                 }
             }}
             />
-            <div className="text-light">
+            <div className="text-light container task">
                 <ul>
                     {toDo.map((item, index) => {
                         return (
-                            <div key={index}className="d-flex">
-                                <li key={index}>{item}</li>
-                                <button
+                            <div key={index}className="d-flex justify-content-between">
+                                <li  className="confortaa" key={index}>{item}</li>
+                                <i class="fa-regular fa-trash-can"
                                     onClick={(event) => {
                                         const newToDo = toDo.filter((filterItem, filterIndex) => {
                                             // return filterIndex != index (it the same)
@@ -43,13 +44,14 @@ const Input = () => {
                                         })   
                                         setToDo(newToDo)
                                     }}
-                                >X</button>
+                                    ></i>
                             </div>
                         )
                     }
                     )}
                 </ul>
             </div>
+            <p className="text-light confortaa">{toDoCounter} item left</p>
         </div>
     );
 };
